7fda62ac1a215f6cf0c633cf2642b408d1deb2ce
Andrea <okurisuzume@wp.pl>
2017-07-26 12:50:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/piotrpiro/inventoryapp/CatalogActivity.java
@@ -0,0 +1,166 @@
+package com.example.piotrpiro.inventoryapp;
+
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.piotrpiro.inventoryapp.data.InventoryContract.InventoryEntry;
+
+/**
+ * Displays list of items that were entered and stored in the app.
+ */
+public class CatalogActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    /** Identifier for the item data loader */
+    private static final int ITEM_LOADER = 0;
+
+    /** Adapter for the ListView */
+    InventoryCursorAdapter mCursorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find the ListView which will be populated with the inventory data
+        ListView itemListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        itemListView.setEmptyView(emptyView);
+
+        // Setup an Adapter to create a list item for each row of item data in the Cursor.
+        // There is no item data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new InventoryCursorAdapter(this, null);
+        itemListView.setAdapter(mCursorAdapter);
+
+        // Setup the item click listener
+        itemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link EditorActivity}
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+
+                // Form the content URI that represents the specific pet that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link InventoryEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.piotrpiro.inventory/inventory/2"
+                // if the item with ID 2 was clicked on.
+                Uri currentItemUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentItemUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current item.
+                startActivity(intent);
+            }
+        });
+
+        // Kick off the loader
+        getLoaderManager().initLoader(ITEM_LOADER, null, this);
+    }
+
+    /**
+     * Helper method to insert hardcoded item data into the database. For debugging purposes only.
+     */
+    private void insertItem() {
+        // Create a ContentValues object where column names are the keys,
+        // and Dove shampoo item attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(InventoryEntry.COLUMN_ITEM_NAME, "Dove shampoo");
+        values.put(InventoryEntry.COLUMN_ITEM_QUANTITY, 8);
+        values.put(InventoryEntry.COLUMN_ITEM_PRICE, 9);
+
+        // Insert a new row for Dove shampoo into the provider using the ContentResolver.
+        // Use the {@link InventoryEntry#CONTENT_URI} to indicate that we want to insert
+        // into the items database table.
+        // Receive the new content URI that will allow us to access Dove shampoo data in the future.
+        getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
+    }
+
+    /**
+     * Helper method to delete all items in the database.
+     */
+    private void deleteAllItems() {
+        int rowsDeleted = getContentResolver().delete(InventoryEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from inventory database");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertItem();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllItems();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_ITEM_QUANTITY,
+                InventoryEntry.COLUMN_ITEM_PRICE };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                InventoryEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update {@link InventoryCursorAdapter} with this new cursor containing updated item data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
+    }
+}
